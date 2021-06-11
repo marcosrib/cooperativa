35c48c724fd6acb28cb846deaaaf1e2b3124a7a7
marcos <marcos.rib.sousa@gmail.com>
2021-06-11 14:54:42 -0300

    refactor(dto): change id field from string to Integer

--- src/main/java/com/cooperativa/api/dtos/PautaDTO.java
+++ src/main/java/com/cooperativa/api/dtos/PautaDTO.java
@@ -1,26 +1,28 @@
 package com.cooperativa.api.dtos;
 
 public class PautaDTO {
-	private String id;
+	private Integer id;
 	private String name;
-	
-	public PautaDTO(String id, String name) {
+
+	public PautaDTO(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	
-	public String getId() {
+
+	public Integer getId() {
 		return id;
 	}
-	public void setId(String id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
