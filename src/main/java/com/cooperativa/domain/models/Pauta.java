d129efdcafe35e6ce485bfc5b82f2a225db87a02
marcos <marcos.rib.sousa@gmail.com>
2021-06-11 14:31:51 -0300

    feat(model): add entity Pauta

---
+++ src/main/java/com/cooperativa/domain/models/Pauta.java
@@ -0,0 +1,48 @@
+package com.cooperativa.domain.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pauta")
+public class Pauta {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	private String name;
+	@Column(name = "createdAt")
+	private LocalDateTime createdAt;
+	@Column(name = "updatedAt")
+	private LocalDateTime updatedAt;
+	
+	public Pauta(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
