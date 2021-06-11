406a86b84ea62ea185c33f0dc70879d89656568b
marcos <marcos.rib.sousa@gmail.com>
2021-06-11 13:11:59 -0300

    feat(services): add serices Affiliated

---
+++ src/main/java/com/cooperativa/services/domain/AffiliatedService.java
@@ -0,0 +1,5 @@
+package com.cooperativa.services.domain;
+
+public interface AffiliatedService {
+
+}
---
+++ src/main/java/com/cooperativa/services/domain/impl/AffiliatedServiceImpl.java
@@ -0,0 +1,5 @@
+package com.cooperativa.services.domain.impl;
+
+public class AffiliatedServiceImpl {
+
+}
