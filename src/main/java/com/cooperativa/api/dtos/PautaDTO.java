0d8dba8b8e08f08c7c7b467ced4fcc72d92db7ef
marcos <marcos.rib.sousa@gmail.com>
2021-06-11 14:37:28 -0300

    feat(dto): add dto pauta

---
+++ src/main/java/com/cooperativa/api/dtos/PautaDTO.java
@@ -0,0 +1,26 @@
+package com.cooperativa.api.dtos;
+
+public class PautaDTO {
+	private String id;
+	private String name;
+	
+	public PautaDTO(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
