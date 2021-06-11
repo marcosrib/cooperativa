df5ac048b73eccc22398916b0f3d097e847d2046
marcos <marcos.rib.sousa@gmail.com>
2021-06-11 13:48:00 -0300

    refactor:(model): add entity Affiliated jpa anotations

--- src/main/java/com/cooperativa/domain/models/Affiliated.java
+++ src/main/java/com/cooperativa/domain/models/Affiliated.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -11,7 +14,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "affiliated")
 public class Affiliated {
-
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String cpf;
 	private String name;
