eacf0eabacb27ae452020d0cadba9eaa070a83b7
root <marcos.rib.sousa@gmail.com>
2021-06-12 16:24:33 -0300

    refactor: alter id Long to Interger

--- src/main/java/com/cooperativa/domain/repositories/AffiliatedRepository.java
+++ src/main/java/com/cooperativa/domain/repositories/AffiliatedRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.cooperativa.domain.models.Affiliated;
 
 @Repository
-public interface AffiliatedRepository extends CrudRepository<Affiliated, Long>{
+public interface AffiliatedRepository extends CrudRepository<Affiliated, Integer>{
 
 }
